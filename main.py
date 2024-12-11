559a50ef07b6177ad8118f35724240a525425b31
Ubuntu <ubuntu@ip-172-31-40-190.ec2.internal>
2024-12-11 01:35:23 +0000

    server version

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ def download():
     return send_file(path, as_attachment=True)
 
 if __name__ =="__main__":
-    app.run(debug=True, host="0.0.0.0", port=8080)
+    app.run(debug=True, host="0.0.0.0", port=9000)
